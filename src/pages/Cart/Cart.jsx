7bab73a063eace8055c5cb871ca7e0a1bd1ec915
Arafath Hussain <mohamedarafath1606@gmail.com>
2025-08-05 21:33:49 +0530

    bug fix

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,8 +3,13 @@ import { StoreContext } from "../../context/StoreContext";
 import "./Cart1.css";
 import { useNavigate } from "react-router-dom";
 const Cart = () => {
-  const { cardItems, food_list, removeFromCart, addToCart,getTotalCartAmount } =
-    useContext(StoreContext);
+  const {
+    cardItems,
+    food_list,
+    removeFromCart,
+    addToCart,
+    getTotalCartAmount,
+  } = useContext(StoreContext);
   const navigate = useNavigate();
   return (
     <div className="cart">
@@ -28,7 +33,7 @@ const Cart = () => {
                   <img src={element.image} alt={element.name} />
                   <p>{element.name}</p>
                   <p>₹ {element.price}</p>
-                  <p className="addOrRemoveBtn"> 
+                  <p className="addOrRemoveBtn">
                     <span>
                       <p
                         className="addOrRemove"
@@ -39,7 +44,7 @@ const Cart = () => {
                     </span>{" "}
                     {quantity}
                     <span>
-                    <p
+                      <p
                         className="addOrRemove"
                         onClick={() => addToCart(element._id)}
                       >
@@ -54,32 +59,36 @@ const Cart = () => {
             );
           }
         })}
-      </div> 
+      </div>
       <div className="cart-bottom">
-        <div  className="cart-total">
+        <div className="cart-total">
           <h2>Cart Totals</h2>
           <div>
             <div className="cart-total-details">
               <p>Sub Total</p>
               <p>₹ {getTotalCartAmount()}</p>
             </div>
-              <hr />
+            <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>₹ {2}</p>
+              <p>₹ {getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>₹ {getTotalCartAmount()+2}</b>
+              <b>
+                ₹ {getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              </b>
             </div>
           </div>
-         <button onClick={()=>navigate('/order')}>Proceed to Checkout</button>
+          <button onClick={() => navigate("/order")}>
+            Proceed to Checkout
+          </button>
         </div>
         <div className="cart-promocode">
           <p>If you have a promo code, Enter it here</p>
           <div className="cart-promo-input">
-            <input type="text"  placeholder="PROMO CODE"/>
+            <input type="text" placeholder="PROMO CODE" />
             <button>Submit</button>
           </div>
         </div>
